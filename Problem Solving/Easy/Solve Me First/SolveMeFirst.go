f771c7ecb53c728d02c72c3816a180bc9ad7b3f9
Syafiqjos <syafiqjos@gmail.com>
2021-10-02 22:29:43 +0700

    Add SolveMeFirst.go Solution

---
+++ Problem Solving/Easy/Solve Me First/SolveMeFirst.go
@@ -0,0 +1,19 @@
+// HackerRank
+// syafiqjos
+// https://www.hackerrank.com/challenges/solve-me-first/problem
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	var b int
+
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+
+	fmt.Println(a + b)
+}
