0fa202dc73251d5d0c91ebc7fd5956fdad84d340
Syafiqjos <syafiqjos@gmail.com>
2021-10-03 18:53:21 +0700

    Add BetweenTwoSets.go Solution

---
+++ Problem Solving/Easy/Between Two Sets/BetweenTwoSets.go
@@ -0,0 +1,137 @@
+// HackerRank
+// syafiqjos
+// https://www.hackerrank.com/challenges/between-two-sets/problem
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int32) int32 {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(integers []int32) int32 {
+	result := integers[0]
+
+	for i := 1; i < len(integers); i++ {
+		result = result * integers[i] / GCD(result, integers[i])
+	}
+
+	return result
+}
+
+func getTotalX(a []int32, b []int32) int32 {
+	// Write your code here
+
+	lcm := LCM(a)
+	gcd := b[0]
+
+	for i := 1; i < len(b); i++ {
+		gcd = GCD(gcd, b[i])
+	}
+
+	fmt.Println(lcm)
+	fmt.Println(gcd)
+
+	acc := lcm
+
+	val := int32(0)
+
+	for acc <= gcd {
+		if gcd%acc == 0 {
+			fmt.Println(acc)
+			val++
+		}
+
+		acc += lcm
+	}
+
+	return val
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	mTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	brrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var brr []int32
+
+	for i := 0; i < int(m); i++ {
+		brrItemTemp, err := strconv.ParseInt(brrTemp[i], 10, 64)
+		checkError(err)
+		brrItem := int32(brrItemTemp)
+		brr = append(brr, brrItem)
+	}
+
+	total := getTotalX(arr, brr)
+
+	fmt.Fprintf(writer, "%d\n", total)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
