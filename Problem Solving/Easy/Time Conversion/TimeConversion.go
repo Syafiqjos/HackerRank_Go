0f9924a3f8bc4f1235088f79f67a5d392a4a23f1
Syafiqjos <syafiqjos@gmail.com>
2021-10-03 10:41:32 +0700

    Add TimeConversion.go Solution

---
+++ Problem Solving/Easy/Time Conversion/TimeConversion.go
@@ -0,0 +1,85 @@
+// HackerRank
+// syafiqjos
+// https://www.hackerrank.com/challenges/time-conversion/problem
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func timeConversion(s string) string {
+	// Write your code here
+	str := s[0 : len(s)-2]
+	split := strings.Split(str, ":")
+
+	ampm := s[len(s)-2:]
+
+	hh, _ := strconv.ParseInt(split[0], 10, 64)
+	mm, _ := strconv.ParseInt(split[1], 10, 64)
+	ss, _ := strconv.ParseInt(split[2], 10, 64)
+
+	if strings.ToLower(ampm) == "am" {
+		if hh == 12 {
+			hh = 0
+		} else {
+
+		}
+	} else if strings.ToLower(ampm) == "pm" {
+		if hh == 12 {
+			hh = 12
+		} else {
+			hh += 12
+		}
+	}
+
+	var output string = fmt.Sprintf("%02v:%02v:%02v", hh, mm, ss)
+
+	return output
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	s := readLine(reader)
+
+	result := timeConversion(s)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
